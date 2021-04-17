d9682b44f7342754e048da4c1154b0c301e189bd
milagrosechague4 <mechague@udesa.edu.ar>
2021-04-17 11:41:15 -0300

    Perros

--- miProyecto/controllers/mainController.js
+++ miProyecto/controllers/mainController.js
@@ -1,6 +1,11 @@
+const rescatados = require('../modulos/rescatados');
+const perrosArray = rescatados.perros;
+const gatosArray = rescatados.gatos;
+
 let controller = {
     index: function(req, res) {
-        return res.render('index', { title: 'Express' });
+        
+        return res.render('index', {perrosArray});
     },
 
     register: function(req, res){
