556fffe9bd61863697e37fda663a85e2f4c09997
Your Name <you@example.com>
2021-04-17 10:38:00 -0300

    Log in

--- miProyecto/controllers/mainController.js
+++ miProyecto/controllers/mainController.js
@@ -5,7 +5,11 @@ let controller = {
 
     register: function(req, res){
         return res.render('register', {title: 'hola'})
-    }
+    },
+
+    login: function(req, res) {
+        return res.render('login', { title: 'Registrarse' });
+    },
 }
 
 module.exports = controller;
