b7a6b2cee704c8fbecea3b6871b0d1f934f633ee
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2021-04-19 17:46:36 -0300

    agregue usuarios

---
+++ miProyecto/modulos/usuarios.js
@@ -0,0 +1,54 @@
+let usuarios = {
+    usuarios: [
+            {
+                id: 1,
+                nombre: "Maria Gutierrez",
+                añoDeNacimiento: 23/7/1987,
+                email: "mgutierrez@gmail.com",
+
+            },
+            {
+                id: 2,
+                nombre: "Franco Beslavsky",
+                añoDeNacimiento: 1/9/1999,
+                email: "beslaf@fibertel.com.ar",
+
+            },
+            {
+                id: 3,
+                nombre: "Santino Benitez",
+                añoDeNacimiento: 2/9/1993,
+                email: "santinob@gmail.com",
+
+             
+            },
+            {
+                id: 4,
+                nombre: "Teresa Martinez",
+                añoDeNacimiento: 19/5/1989,
+                email: "teremartinez@gmail.com",
+
+                           
+            },
+            {
+                id: 5,
+                nombre: "Lucas Fernandez",
+                añoDeNacimiento: 8/1/1999,
+                email: "luquitas8@gmail.com",
+            },
+            {
+                id: 6,
+                nombre: "Marcos Aramburu",
+                añoDeNacimiento: 18/7/1987,
+                email: "marcosgolf1@gmail.com",
+            },
+            {
+                id: 7,
+                nombre: "Ana Teresa",
+                añoDeNacimiento: 30/9/1993,
+                email: "anatere@gmail.com",
+            },],
+
+        }
+
+module.exports = usuarios;
