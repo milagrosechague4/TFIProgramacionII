d6c3299011d753231fd2edfa066e279c4093d994
milagrosechague4 <mechague@udesa.edu.ar>
2021-04-18 21:20:32 -0300

    ruta product

--- miProyecto/controllers/productController.js
+++ miProyecto/controllers/productController.js
@@ -2,10 +2,21 @@ const rescatados = require('../modulos/rescatados');
 const perrosArray = rescatados.perros;
 const gatosArray = rescatados.gatos;
 
-let controller = { 
-    index: function(req, res) {
-        return res.render('product');
+module.exports = { 
+   // index: function(req, res) {
+   //     return res.render('product');
+   // },
+
+    id : (req, res)=> {
+        let id = req.params.id
+
+        for(let i = 0; i < perrosArray.length; i++){
+            if(perrosArray[i].id == id){
+                let perro = perrosArray[i]
+                return res.render('product', {perro});
+            } 
+        }
     }
 }
 
-module.exports = controller;
+
--- miProyecto/routes/product.js
+++ miProyecto/routes/product.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var productController = require('../controllers/productController');
 
 /* GET home page. */ 
-router.get('/', productController.index);
+//router.get('/', productController.index);
+router.get('/product/:id', productController.id )
 
 module.exports = router;
