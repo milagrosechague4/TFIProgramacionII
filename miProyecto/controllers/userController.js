f697d5e2aa382cb80e25e6aa0ad5e38380172352
milagrosechague4 <mechague@udesa.edu.ar>
2021-04-25 22:06:41 -0300

    ultimo commit

--- miProyecto/controllers/userController.js
+++ miProyecto/controllers/userController.js
@@ -6,10 +6,10 @@ const usuariosArray = usuarios.index
 
 module.exports = {
 
-//perfil: (req, res)=>{
-//    let usuario = usuariosArray
- //   return res.render('profile', {usuario, animalesArray})
-//},
+perfil: (req, res)=>{
+    let usuario = usuariosArray[0]
+   return res.render('profile', {usuario, animalesArray})
+},
 
 users: function(req, res){
     let id = req.params.id
@@ -25,7 +25,17 @@ users: function(req, res){
 },
 
 userEdit: function(req, res) {
-    return res.render('profile-edit', { title: 'editar perfil' });
+
+    let id = req.params.id
+    let usuario ;
+
+    for(let i = 0; i < usuariosArray.length; i++){
+        if(usuariosArray[i].id == id){
+            usuario = usuariosArray[i]
+        } 
+    }
+
+    return res.render('profile-edit', {usuario});
 }, 
 
 }
